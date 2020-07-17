b0c616b11b7913e5adf642f478fc844b871a9707
huangliang <huangliang@yinhai.com>
2020-07-17 16:49:37 +0800

    优化方法，增加测试

--- day05/main.py
+++ day05/main.py
@@ -4,10 +4,9 @@ from day05.csv_reader import CsvReader
 from day05.zip_reader import ZipReader
 
 
-def test_txt_from_zip():
-    zip_reader = ZipReader("file/people.zip")
-    zip_reader.extract_zip("zip")
-    txt_reader = TxtReader("zip/people.txt")
+def test_txt():
+    # txt文件读取测试
+    txt_reader = TxtReader("file/people.txt")
     joseph = Joseph()
     people_list = txt_reader.get_people_list()
     joseph.append(people_list)
@@ -17,10 +16,9 @@ def test_txt_from_zip():
     assert last_people.number == 4
 
 
-def test_csv_from_zip():
-    zip_reader = ZipReader("file/people.zip")
-    zip_reader.extract_zip("zip")
-    csv_reader = CsvReader("zip/people.csv")
+def test_csv():
+    # csv文件读取测试
+    csv_reader = CsvReader("file/people.csv")
     joseph = Joseph()
     people_list = csv_reader.get_people_list()
     joseph.append(people_list)
@@ -31,9 +29,8 @@ def test_csv_from_zip():
 
 
 def test_csv_with_wrong_file():
-    zip_reader = ZipReader("file/people.zip")
-    zip_reader.extract_zip("zip")
-    csv_reader = CsvReader("zip/people.txt")
+    # 用csv读取txt文件测试
+    csv_reader = CsvReader("file/people.txt")
     joseph = Joseph()
     people_list = csv_reader.get_people_list()
     joseph.append(people_list)
@@ -43,11 +40,42 @@ def test_csv_with_wrong_file():
     assert last_people.number == 4
 
 
+def test_txt_from_zip():
+    # 测试直接读取zip中的txt文件
+    zip_reader = ZipReader("file/people.zip")
+    joseph = Joseph()
+    people_list = zip_reader.get_people_list_from_txt("people.txt")
+    joseph.append(people_list)
+    step = 3
+    start_number = 0
+    last_people = joseph.get_last_people(step, start_number)
+    assert last_people.number == 4
+
+
+def test_people_from_different():
+    txt_reader = TxtReader("file/people.txt")
+    txt_people_list = txt_reader.get_people_list()
+    csv_reader = CsvReader("file/people.csv")
+    csv_people_list = csv_reader.get_people_list()
+    zip_reader = ZipReader("file/people.zip")
+    zip_people_list = zip_reader.get_people_list_from_txt("people.txt")
+    joseph = Joseph()
+    joseph.append(txt_people_list)
+    joseph.append(csv_people_list)
+    joseph.append(zip_people_list)
+    step = 4
+    start_number = 13
+    last_people = joseph.get_last_people(step, start_number)
+    print(last_people)
+
+
 if __name__ == "__main__":
     try:
+        # test_txt()
+        # test_csv()
+        # test_csv_with_wrong_file()
         # test_txt_from_zip()
-        # test_csv_from_zip()
-        test_csv_with_wrong_file()
+        test_people_from_different()
     except ValueError as ve:
         print(ve)
     except IndexError as ie:
--- day05/zip_reader.py
+++ day05/zip_reader.py
@@ -1,4 +1,6 @@
 import zipfile
+import json
+from day04.people import People
 
 
 class ZipReader(object):
@@ -6,28 +8,56 @@ class ZipReader(object):
     zip文件读取类，包含功能：
     1.查看压缩包有哪些文件
     2.解压压缩包到自定文件
+    3.直接读取文件里面csv内容返回people_list
+    4.直接读取文件里面txt内容返回people_list
     """
 
     def __init__(self, file_path):
         self._file_path = file_path
 
     def get_zip_list(self):
-        with zipfile.ZipFile(self._file_path, 'r') as zip:
-            file_list = zip.namelist()
+        with zipfile.ZipFile(self._file_path, 'r') as zp:
+            file_list = zp.namelist()
             print(file_list)
 
     def extract_zip(self, extract_path):
-        with zipfile.ZipFile(self._file_path, 'r') as zip:
-            file_list = zip.namelist()
+        with zipfile.ZipFile(self._file_path, 'r') as zp:
+            file_list = zp.namelist()
             for file in file_list:
-                zip.extract(file, extract_path)
+                zp.extract(file, extract_path)
+
+    def get_people_list_from_csv(self, csv_name):
+        people_list = []
+        with zipfile.ZipFile(self._file_path, 'r') as zp:
+            info = zp.read(csv_name).decode("UTF-8").split("\n")
+            for str_people in info:
+                str_list = str_people.split(",")
+                people = People(str_list[0].replace("\"", ""), int(str_list[1]), int(str_list[2]), int(str_list[3]))
+                people_list.append(people)
+        return people_list
+
+    def get_people_list_from_txt(self, txt_name):
+        people_list = []
+        with zipfile.ZipFile(self._file_path, 'r') as zp:
+            info = zp.read(txt_name).decode("UTF-8").split("\n")
+            for str_people in info:
+                people = json.loads(str_people, object_hook=lambda x: People(x['name'], x['number'], x['age'], x['sex']))
+                people_list.append(people)
+        return people_list
 
 
-def test_zip():
+def test_zip_txt():
     zip_reader = ZipReader("file/people.zip")
-    zip_reader.get_zip_list()
-    zip_reader.extract_zip("zip")
+    people_list = zip_reader.get_people_list_from_txt("people.txt")
+    print(people_list)
+
+
+def test_zip_csv():
+    zip_reader = ZipReader("file/people.zip")
+    people_list = zip_reader.get_people_list_from_csv("people.csv")
+    # print(people_list)
 
 
 if __name__ == "__main__":
-    test_zip()
+    # test_zip_txt()
+    test_zip_csv()
